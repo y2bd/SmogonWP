1af264de3fbeb9134ccffa14f15f9916c1dcedfe
y2bd <jason@y2bd.me>
2013-10-19 19:53:04 -0700

    initial content commit

---
+++ SmogonWP/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using SmogonWP.Resources;
+
+namespace SmogonWP
+{
+  /// <summary>
+  /// Provides access to string resources.
+  /// </summary>
+  public class LocalizedStrings
+  {
+    private static AppResources _localizedResources = new AppResources();
+
+    public AppResources LocalizedResources { get { return _localizedResources; } }
+  }
+}
