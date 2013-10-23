f2e34a66904ece89b5821bedd0caa3d6924a1709
y2bd <jason@y2bd.me>
2013-10-23 15:17:37 -0700

    added ability data objs

---
+++ libs/schmogon/Schmogon/Data/Abilities/Ability.cs
@@ -0,0 +1,18 @@
+﻿namespace Schmogon.Data.Abilities
+{
+  public class Ability : ISearchItem
+  {
+    public Ability(string name, string description, string pageLocation)
+    {
+      Name = name;
+      Description = description;
+      PageLocation = pageLocation;
+    }
+
+    public string Name { get; private set; }
+
+    public string Description { get; private set; }
+
+    public string PageLocation { get; private set; }
+  }
+}
