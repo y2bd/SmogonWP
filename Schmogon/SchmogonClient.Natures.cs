427775931554c5928c77a941975960363a0463d7
y2bd <jason@y2bd.me>
2013-10-24 14:55:12 -0700

    added natures and their effects

--- Schmogon/SchmogonClient.Natures.cs
+++ Schmogon/SchmogonClient.Natures.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Schmogon.Data.Natures;
+using Schmogon.Data.Stats;
 
 namespace Schmogon
 {
   public partial class SchmogonClient
   {
+    public IEnumerable<NatureEffect> GetAllNatureEffects()
+    {
+      return NatureEffect.NatureEffects;
+    }
+
+    public NatureEffect GetNatureEffect(Nature nature)
+    {
+      return NatureEffect.NatureEffects.First(n => n.Nature == nature);
+    }
+
+    public IEnumerable<NatureEffect> GetNatureEffectWhere(StatType increased, StatType decreased)
+    {
+      return NatureEffect.NatureEffects.Where(n => n.Increased == increased &&
+                                              n.Decreased == decreased &&
+                                              !n.IsNeutral);
+    }
+
+    public IEnumerable<NatureEffect> GetNatureEffectWhereIncreased(StatType increased)
+    {
+      return NatureEffect.NatureEffects.Where(n => n.Increased == increased &&
+                                              !n.IsNeutral);
+    }
+
+    public IEnumerable<NatureEffect> GetNatureEffectWhereDecreased(StatType decreased)
+    {
+      return NatureEffect.NatureEffects.Where(n => n.Decreased == decreased &&
+                                              !n.IsNeutral);
+    }
+
   }
 }
--- Schmogon/SchmogonClient.cs
+++ Schmogon/SchmogonClient.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace Schmogon
 {
@@ -12,5 +10,12 @@ namespace Schmogon
 
     private const string DescHeader = "Description";
     private const string CompHeader = "Competitive Use";
+
+    private static string processUL(HtmlNode list)
+    {
+      if (list.Name.Equals("ul") != true) throw new ArgumentException("param must be of the node type UL", "list");
+
+      return list.Elements("li").Aggregate(string.Empty, (current, elem) => current + ("* " + elem.InnerText.Trim() + "\n"));
+    }
   }
 }
