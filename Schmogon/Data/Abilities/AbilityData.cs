60155ddcd03ec9bc0593ab79c48004f4db84f7b8
y2bd <jason@y2bd.me>
2013-10-31 16:11:36 -0700

    Squashed 'libs/schmogon/' content from commit 9c3dc21
    git-subtree-dir: libs/schmogon
    git-subtree-split: 9c3dc210d066de6e200b70f504c4b740d5d3a716

---
+++ Schmogon/Data/Abilities/AbilityData.cs
@@ -0,0 +1,16 @@
+﻿namespace Schmogon.Data.Abilities
+{
+  public class AbilityData
+  {
+    public AbilityData(string name, string description, string competitive)
+    {
+      Name = name;
+      Description = description;
+      Competitive = competitive;
+    }
+
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public string Competitive { get; private set; }
+  }
+}
---
+++ Schmogon/ISchmogonClient.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Schmogon.Data.Abilities;
+using Schmogon.Data.Moves;
+using Schmogon.Data.Natures;
+using Schmogon.Data.Stats;
+using Schmogon.Data.Types;
+
+namespace Schmogon
+{
+  public interface ISchmogonClient
+  {
+    Task<IEnumerable<Move>> GetAllMovesAsync();
+
+    Task<IEnumerable<Move>> SearchMovesAsync(string query);
+
+    Task<MoveData> GetMoveDataAsync(Move move);
+
+    Task<IEnumerable<Ability>> GetAllAbilitiesAsync();
+
+    Task<IEnumerable<Ability>> SearchAbilitiesAsync(string query);
+
+    Task<AbilityData> GetAbilityDataAsync(Ability ability);
+
+    IEnumerable<NatureEffect> GetAllNatureEffects();
+
+    NatureEffect GetNatureEffect(Nature nature);
+
+    IEnumerable<NatureEffect> GetNatureEffectsWhere(StatType increased, StatType decreased);
+
+    IEnumerable<NatureEffect> GetNatureEffectsWhereIncreased(StatType increased);
+
+    IEnumerable<NatureEffect> GetNatureEffectsWhereDecreased(StatType decreased);
+
+    IEnumerable<TypeOffenseEffect> GetAllTypeOffenseEffects();
+
+    IEnumerable<TypeDefenseEffect> GetAllTypeDefenseEffects();
+
+    TypeOffenseEffect GetTypeOffenseEffect(Type type);
+
+    TypeDefenseEffect GetTypeDefenseEffect(Type type);
+  }
+}
