ac18db281acf9762808607642fd01ed55f8b63e9
Jason Lo <jason@y2bd.me>
2014-01-21 13:42:23 -0800

    fixed transition bug (?)

--- SmogonWP/App.xaml.cs
+++ SmogonWP/App.xaml.cs
@@ -218,7 +218,7 @@ namespace SmogonWP
       
       // Create the frame but don't set it as RootVisual yet; this allows the splash
       // screen to remain active until the application is ready to render.
-      RootFrame = new TransitionFrameSlim();
+      RootFrame = new TransitionFrame();
       RootFrame.Navigated += CompleteInitializePhoneApplication;
 
       // Handle navigation failures
